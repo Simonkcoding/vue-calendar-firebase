9bb691a64bb4b145f2e042f8c8ba88e3bce10bc9
Yinlung Kwok <yinlung.kwok@cognizant.com>
2019-12-03 11:26:04 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyBlDIulrJ_QxpnDt7QiaT9dK0zcJ2Qz_N0",
+  authDomain: "vue-calendar-a0209.firebaseapp.com",
+  databaseURL: "https://vue-calendar-a0209.firebaseio.com",
+  projectId: "vue-calendar-a0209",
+  storageBucket: "vue-calendar-a0209.appspot.com",
+  messagingSenderId: "1020109723597",
+  appId: "1:1020109723597:web:bb1295af27814f77559684"
+});
+
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
